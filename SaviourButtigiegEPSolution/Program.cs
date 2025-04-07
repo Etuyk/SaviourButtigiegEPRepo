e733ab2d3e55b991dd073a2c8d4c6d3a5bbdf6f6
Saviour Buttigieg <sambuttigieg55@gmail.com>
2025-04-07 14:02:53 +0200

    Added poll results. They are showing via a chart in each vote

--- SaviourButtigiegEPSolution/Program.cs
+++ SaviourButtigiegEPSolution/Program.cs
@@ -35,6 +35,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Poll}/{action=Index}/{id?}");
 
 app.Run();
